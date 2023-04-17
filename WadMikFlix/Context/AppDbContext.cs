f8cf1005bf15a80d5aef8208fe8cd35a9c18855c
Mikhail-Aurelio <mikhail.aurelio@gmail.com>
2023-04-17 20:22:43 -0300

    configuração de dbcontext

---
+++ WadMikFlix/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WadMikFlix.Models;
+
+namespace WadMikFlix.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Genero>  Generos { get; set; }
+        public DbSet<Filme>  Filmes { get; set; }
+    }
+}
--- WadMikFlix/Program.cs
+++ WadMikFlix/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WadMikFlix.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
