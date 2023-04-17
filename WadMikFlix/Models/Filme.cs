b222bd8b86a13cd6a3dd6ddb9d98a93392f6d848
Wadson92 <wadson.dev92@gmail.com>
2023-04-17 19:44:50 -0300

    model filme e genero

---
+++ WadMikFlix/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WadMikFlix.Models
+{
+    public class Filme
+    {
+        public int FilmeId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public int Classificacao { get; set; }
+
+        public int GeneroId { get; set; }
+        public virtual Genero Genero { get; set; }
+    }
+}
--- WadMikFlix/Models/Genero.cs
+++ WadMikFlix/Models/Genero.cs
@@ -1,11 +1,15 @@
-﻿namespace WadMikFlix.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WadMikFlix.Models
 {
     public class Genero
     {
         public int GeneroId { get; set; }
 
+        [Required]
         public string Nome { get; set; }
 
+        public virtual List<Filme> FilmeId { get; set; }
 
     }
 }
