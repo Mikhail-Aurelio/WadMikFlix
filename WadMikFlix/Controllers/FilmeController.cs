ebc3bbe4cfcdd091a66f55475b164d6eaf216d61
Wadson92 <wadson.dev92@gmail.com>
2023-04-18 20:54:52 -0300

    Cria Controller

---
+++ WadMikFlix/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WadMikFlix.Context;
+
+namespace WadMikFlix.Controllers
+{
+    public class FilmeController : Controller
+    {
+        private readonly AppDbContext _context;
+        public FilmeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var teste = _context.Filmes;
+            return View();
+        }
+    }
+}
